2624728df00209930f65a3908064ecb2c5507924
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-01-19 08:50:40 +0700

    init

---
+++ constants/index.js
@@ -0,0 +1,90 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/planet-01.png",
+    title: "One Stop Solution toolkit",
+  },
+  {
+    id: "world-2",
+    imgUrl: "/planet-02.png",
+    title: "State Based Code Editor",
+  },
+  {
+    id: "world-3",
+    imgUrl: "/planet-03.png",
+    title: "Completely Free",
+  },
+  {
+    id: "world-4",
+    imgUrl: "/planet-04.png",
+    title: "Powered by Da NextJS Framework ",
+  },
+  {
+    id: "world-5",
+    imgUrl: "/planet-05.png",
+    title: "AI Powered AMA",
+  },
+];
+
+export const startingFeatures = [
+  "Pilih Course yang ingin kamu pelajari",
+  "Latih kemampuan koding dengan Fitur State Based Code Editor Kodekan",
+  "Bila kamu bingung jangan khawatir, Fitur AMA (Ask Me Anything) siap membantu kamu",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: "/vrpano.svg",
+    title: "Added Oauth Support",
+    subtitle:
+      "Kami tau kamu malas, maka gunakan oauth sebagai metode login bagi kamu si paling malas",
+  },
+  {
+    imgUrl: "/headset.svg",
+    title: "UI/UX Improvement",
+    subtitle:
+      "Di tenagai oleh da powa of da only nextjs 13, kodekan membawa fitur hybrid ssg/ssr demi kamu si paling benci lola ( Loading Lama )",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/planet-06.png",
+    title: "Don't try to Learn Everything, learn just enough",
+    subtitle:
+      "pelajari secukupnya, dan langsung coba buat sesuatu dari apa yang telah kamu pelajari",
+  },
+  {
+    imgUrl: "/planet-07.png",
+    title:
+      "Brute Force is not the answer, sometimes it's necessary to step back a little",
+    subtitle:
+      "kamu belajar ngoding 10 jam sehari ? tapi ga jago - jago ? mungkin kamu perlu rehat sejenak supaya engine kamu dingin dan bisa menyerap informasi dengan lebih efisien",
+  },
+  {
+    imgUrl: "/planet-08.png",
+    title:
+      "trying to learn to code is all about grind, u can't be a pro in a single night",
+    subtitle:
+      "it's all about grind mas bro, di setiap coding problem yang kamu temui adalah sebuah langkah maju dalam meningkatkan skill mastery ngoding kamu",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/facebook.svg",
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}>
+      <TypingText title="| Tentang Kodeverse" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white">
+        <span className="font-extrabold text-white">Kodeverse</span> adalah ide
+        awal dan alasan dibangunnya kodekan, sebuah platform yang akan menjadi{" "}
+        <span className="font-extrabold text-white">one stop solution</span>{" "}
+        bagi mereka yang ingin belajar ngoding di era pembelajaran daring,
+        dimana <span className="font-extrabold text-white">Pengguna</span> dapat
+        mempelajari skill - skill baru dengan lebih efisien melalui fitur dan
+        alat yang tersedia pada platform,{" "}
+        <span className="font-extrabold text-white"> jelajahi</span> apa yang
+        kami tawarkan dengan cara scroll kebawah
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
