f16c8846cacbefbb82b84fcf213bbe4d4a133f84
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-01-19 20:39:21 +0700

    normalisasi sound alarm

--- components/pomodoro/Alarm.js
+++ components/pomodoro/Alarm.js
@@ -3,8 +3,8 @@ import React, { memo } from "react";
 const Alarm = React.forwardRef((_, ref) => {
   return (
     <audio ref={ref}>
-      {/* <source src="/alarm.mp3" type="audio/mp3" /> */}
-      <source src="/wibu-v-2.mp3" type="audio/mp3" />
+      <source src="/alarm.mp3" type="audio/mp3" />
+      {/* <source src="/wibu-v-2.mp3" type="audio/mp3" /> */}
       Browsermu lawas bre ga support elemen audio.
     </audio>
   );
